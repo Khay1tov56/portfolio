11f19e8f81401a5702f9ac966b7b92dce8a5ecf2
NurilloOkeng5670 <khayitov5670@gmail.com>
2022-08-22 09:24:02 +0300

    Add

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// one slider start
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 1050,
+        smartSpeed: 1500,
+        autoplayHoverPause: true,
+
+    });
+})
+
+// one slider end
